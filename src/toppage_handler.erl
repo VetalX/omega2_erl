6eddf25e0458e0df88594d8703a8264a5e6c2c4d
VetalX <Serakov.Vitaliy@gmail.com>
2017-12-12 20:19:18 +0000

    added select data from database and convert to json

--- src/toppage_handler.erl
+++ src/toppage_handler.erl
@@ -3,6 +3,10 @@
 %% @doc Hello world handler.
 -module(toppage_handler).
 
+-record(state, {
+	     pg_conn
+	 }). 
+
 -export([init/3]).
 -export([handle/2]).
 -export([terminate/3]).
@@ -10,18 +14,30 @@
 init(_Type, Req, []) ->
 	{ok, DbHost} = application:get_env(omega2_erl, db_host),
 	{ok, DbUser} = application:get_env(omega2_erl, db_user),
-	{ok, DbPassword} = application:get_enc(omega2_erl, db_password),
+	{ok, DbPassword} = application:get_env(omega2_erl, db_password),
 	{ok, DbDatabase} = application:get_env(omega2_erl, db_database),
-	{ok, Conn} = epgsql:connect(DbHost, DbUser, DbPassword, [{database, DbDatabase}, {timeout, 4000}]),
-	io:format("Conn ~p", [Conn]),
+	{ok, PgConn} = epgsql:connect(DbHost, DbUser, DbPassword, [{database, DbDatabase}, {timeout, 4000}]),
+	{ok, Req, #state{pg_conn = PgConn}}.
 
-	{ok, Req, undefined}.
+handle(Req, #state{pg_conn = PgConn} = State) ->
+	io:format("State ~p~n", [State]),
+	{ok, Cols, Rows} = 
+            epgsql:squery(PgConn, "select temperature, 
+                                          humidity, 
+                                          to_char(dt, 'hh24:mi') as time
+                                     from dht22 
+			            order by id desc 
+			            limit 100;"),
+	Columns = [Col || {column,Col,_,_,_,_} <- Cols],
+	Propl = lists:foldl(fun(R, Acc) -> 
+		               [lists:zip(Columns, tuple_to_list(R)) | Acc]
+			    end, [], Rows),
 
-handle(Req, State) ->
-	io:format("State ~p", [State]),
+	io:format("DbRes ~p~n", [jsx:encode(Propl)]),
+
 	{ok, Req2} = cowboy_req:reply(200, [
-		{<<"content-type">>, <<"text/plain">>}
-	], <<"Hello world!">>, Req),
+		{<<"content-type">>, <<"application/json">>}
+	], jsx:encode(Propl), Req),
 	{ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
